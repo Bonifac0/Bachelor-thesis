75469eb9600edd89a727f776b7887bed78f3daeb
Bonifac0 <kolenomi@seznam.cz>
2025-10-08 19:57:37 +0200

    org name to processed database

--- src/database_processor/make_database.py
+++ src/database_processor/make_database.py
@@ -19,6 +19,7 @@ output_path = "datasets/processed_dataset.json"
 
 def get_uniref(tempura: dict, save_sequence=False):
     # >UniRef50_UPI002E2621C6 uncharacterized protein LOC134193701 n=1 Tax=Corticium candelabrum TaxID=121492 RepID=UPI002E2621C6
+    print()
     with open(UNIREF_PATH, "r") as orgprot_file:
         genes: dict = {}
         counter = 0
@@ -27,10 +28,7 @@ def get_uniref(tempura: dict, save_sequence=False):
             if line.startswith(">"):
                 counter += 1
                 if counter % 100000 == 0:
-                    print(f"Processed {counter / 1000000}M uniref...")
-
-                # if counter > 2000000:
-                #     return genes
+                    print(f"Processed {counter / 1000000}M / 70.1M uniref", end="\r")
 
                 pattern = r"UniRef50_(\S+).*?Tax=(.*?) TaxID=(\d+)"
                 match = re.search(pattern, line)
@@ -39,7 +37,9 @@ def get_uniref(tempura: dict, save_sequence=False):
                     last_prot_id = match.group(1)  # UPI002E2621C6
                     if int(match.group(3)) in tempura:
                         genes[last_prot_id] = {}
-                        genes[last_prot_id]["temp"] = tempura[int(match.group(3))]
+                        genes[last_prot_id]["temp"] = tempura[int(match.group(3))][0]
+                        genes[last_prot_id]["org"] = tempura[int(match.group(3))][1]
+                        genes[last_prot_id]["org_id"] = int(match.group(3))
 
                 else:  # Probably not known organism
                     last_prot_id = None
@@ -49,19 +49,24 @@ def get_uniref(tempura: dict, save_sequence=False):
                     genes[last_prot_id]["sequence"] = line.strip()
                 else:
                     genes[last_prot_id]["sequence"] += line.strip()
-        return genes
+    print()
+    return genes
 
 
 def get_temp():
     with open(TEMP_PATH, "r") as temp_file:
         orgs: dict = {}
         for instance in json.load(temp_file):
-            orgs[instance["taxonomy_id"]] = instance["Topt_ave"]
+            orgs[instance["taxonomy_id"]] = (
+                instance["Topt_ave"],
+                instance["genus_and_species"],
+            )
         return orgs
 
 
 def get_Pfam(prot_temp: dict, split_lim: int):
     # >A0A671U9Z5_SPAAU/41-288 A0A671U9Z5.1 PF00001.26;7tm_1;
+    print()
     output: dict = {}
     counter = 0
     with open(FASTA_PATH, "r") as fasta:
@@ -69,14 +74,12 @@ def get_Pfam(prot_temp: dict, split_lim: int):
             if line.startswith(">"):
                 counter += 1
                 if counter % 100000 == 0:
-                    print(f"Processed {counter / 1000000}M Pfam...")
+                    print(f"Processed {counter / 1000000}M / 57.5M Pfam", end="\r")
 
                 data = line.split()
                 fami_id = data[2].split(".")[0]
                 prot_id = data[1].split(".")[0]
                 if fami_id not in output:
-                    # if counter > split_lim:
-                    #     return output
                     output[fami_id] = {}
 
                 if prot_id in prot_temp:
@@ -88,15 +91,18 @@ def get_Pfam(prot_temp: dict, split_lim: int):
                         output[fami_id][prot_id]["pfam_sec"] = line.strip()
                     else:
                         output[fami_id][prot_id]["pfam_sec"] += line.strip()
+    print()
     return output
 
 
 def main():
     if os.path.exists(prot_temp_path):
+        print(f"Loading prot-temp from {prot_temp_path}")
         with open(prot_temp_path, "r") as f:
             org = json.load(f)
     else:
         org = get_uniref(get_temp())
+        print(f"Writing prot-temp to {prot_temp_path}")
         with open(prot_temp_path, "w") as f:
             json.dump(org, f, indent=4)
 
@@ -105,7 +111,7 @@ def main():
     print("Removing keys with no value")
     fami = {k: v for k, v in fami.items() if v}
 
-    print("Saving to file")
+    print(f"Saving processed databese to {output_path}")
     with open(output_path, "w") as output:
         json.dump(fami, output, indent=4)
 
