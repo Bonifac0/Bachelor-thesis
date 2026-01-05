9b792ad74f795eb364e9fe6e0ac26bf006bbbc35
Bonifac0 <kolenomi@seznam.cz>
2026-01-06 00:02:59 +0100

    add naive mutation bulk

--- src/training/naive_mutation_generator.py
+++ src/training/naive_mutation_generator.py
@@ -1,6 +1,7 @@
 from src.predictor import Classificator
 from src.heplers.levenshtein import levenshtein
 import numpy as np
+import random
 
 
 """
@@ -9,6 +10,24 @@ python -m src.training.naive_mutation_generator
 
 because pyhon need to load packages
 """
+
+AMINO_ACIDS = "ACDEFGHIKLMNPQRSTVWY"
+
+
+def random_single_mutants(sequence: str, n: int = 10) -> list[str]:
+    seen = set()
+    mutants = []
+
+    while len(mutants) < n:
+        pos = random.randrange(len(sequence))
+        new_aa = random.choice([aa for aa in AMINO_ACIDS if aa != sequence[pos]])
+
+        mutant = sequence[:pos] + new_aa + sequence[pos + 1 :]
+        if mutant not in seen:
+            seen.add(mutant)
+            mutants.append(mutant)
+
+    return mutants
 
 
 def single_revert(baseline: str, modified: str) -> list[str]:
@@ -26,15 +45,36 @@ def single_revert(baseline: str, modified: str) -> list[str]:
     return variants
 
 
-def bulk(
+def bulk(  # TODO OPTIONAL add option to overcome local minimum
     mdl: Classificator, baseline: str, top_threshold: float = 0.9, batchsize: int = 10
-) -> str:
+) -> str | None:
     """
     Adding mutation to move to thermoclass
     """
-    for cycle in range(20):  # max cycles
-        pass
-    return "MGRGKSKWFNNRKGYG"
+    best_mutant = baseline
+    best_score: float = mdl.classify([("", baseline)])[0][3]
+    print(f"baseline: {best_score}")
+
+    for cycle in range(min(20, len(baseline))):  # max cycles
+        better_mutants = random_single_mutants(best_mutant, batchsize)
+        props = mdl.classify([("", i) for i in better_mutants])
+        best_index = np.argmax([i[3] for i in props])  # most hyperthermophilic
+
+        if props[best_index][3] >= top_threshold:
+            print(f"number of cycles: {cycle}")
+            print(f"number of mutations: {levenshtein(best_mutant, baseline)}")
+            return best_mutant
+        else:
+            if props[best_index][3] > best_score:
+                best_mutant = better_mutants[best_index]
+                best_score = props[best_index][3]
+                print(props[best_index][3])
+            else:
+                print("better not found in this iteration")
+            # print([props[i][3] for i in range(len(props))], best_index)
+
+        # print(f"best score: {best_score}")
+    return None  # isnt possible to mutate enough
 
 
 def cut(  # TODO check if work corectlly
@@ -44,7 +84,7 @@ def cut(  # TODO check if work corectlly
     bottom_threshold: float = 0.8,
 ) -> str:
     """
-    Cutting mutation while keaping thermoclass
+    Cutting mutation while keeping thermoclass
     """
     minimal_mutant = heavy_mutant
     for cycle in range(levenshtein(heavy_mutant, baseline)):
@@ -54,9 +94,11 @@ def cut(  # TODO check if work corectlly
 
         if props[best_index][3] >= bottom_threshold:
             minimal_mutant = lighter_mutants[best_index]
-            print([props[i][3] for i in range(len(props))], best_index)
+            print(props[best_index][3])
+            # print([props[i][3] for i in range(len(props))], best_index)
         else:
             break
+    print(f"final number of mutations: {levenshtein(minimal_mutant, baseline)}")
     return minimal_mutant
 
 
@@ -71,11 +113,15 @@ def main(mdl: Classificator):
         #     "MLEGKVKWFNSEKGFGFIEVEG",
         # ),
     ]
+    # MRRGKVVWWNIEKGYG
 
     for baseline in example_inp:
-        print(f"baseline: {mdl.classify([baseline])[0][3]}")
-        heavy_mutant = bulk(mdl, baseline[1])
-        minimal_mutant = cut(mdl, heavy_mutant, baseline[1])
+        heavy_mutant = bulk(mdl, baseline[1], 0.8)
+        if heavy_mutant is None:
+            print(f"protein '{baseline[0]}' cannot be mutate enough")
+            continue
+        minimal_mutant = cut(mdl, heavy_mutant, baseline[1], 0.65)
+        print(baseline[1])
         print(minimal_mutant)
     # outputs = mdl.classify(example_inp)
 
