e34089fcc89d66df3739ca536e9240ac23b7850a
Bonifac <kolenomi@seznam.cz>
2026-01-17 17:52:06 +0100

    add training dataset creator

---
+++ src/training/captum_for_training.py
@@ -0,0 +1,84 @@
+from captum.attr import IntegratedGradients
+from src.predictor import Classificator
+from src.heplers.print_eta import ETA
+import torch
+import json
+import numpy as np
+
+
+"""
+to run:
+python -m src.training.captum_for_training
+
+because pyhon need to load packages
+"""
+
+
+def captum(mdl: Classificator, inp: str) -> np.ndarray:
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    ig = IntegratedGradients(mdl.model.forward_embedding)
+
+    _, _, tokens = mdl.batch_converter([("", inp)])
+    embedding = mdl.model.embedding(tokens.to(DEVICE))
+    attr, _ = ig.attribute(
+        embedding,
+        target=3,
+        return_convergence_delta=True,
+        internal_batch_size=12,
+        # n_steps=10,
+    )
+    # print(attr.squeeze(dim=0).shape)
+    return attr.squeeze(dim=0).detach().numpy()[1:-1]
+
+
+def compute_importance(baseline: str, mutant: str) -> np.ndarray:
+    assert len(baseline) == len(mutant)
+    return np.fromiter(
+        (b != m for b, m in zip(baseline, mutant)), dtype=np.uint8, count=len(baseline)
+    )
+
+
+def main(mdl: Classificator):
+    with open(INPUT_PATH, "r") as f:
+        protein_list = json.load(f)
+
+    total_residues = sum(len(prot["domain"]) for prot in protein_list)
+    print(f"Total residues: {total_residues}")
+    X = np.memmap("X.dat", dtype=np.float16, mode="w+", shape=(total_residues, 1280))
+    y = np.memmap("y.dat", dtype=np.uint8, mode="w+", shape=(total_residues,))
+    idx = 0
+
+    protein_count = len(protein_list)
+    eta = ETA(protein_count)
+
+    print()
+    print(
+        f"Processing protein {0}/{protein_count} | ETA: ?",
+        end="\r",
+    )
+    for i, entery in enumerate(protein_list):
+        embedding = captum(mdl, entery["domain"])
+        n = embedding.shape[0]
+        # print(n)
+        # print(len(entery["domain"]))
+
+        X[idx : idx + n] = embedding
+        y[idx : idx + n] = compute_importance(entery["domain"], entery["mutant"])
+        idx += n
+
+        print(
+            f"Processing protein {i + 1}/{protein_count} {eta.print_eta(i + 1)}",
+            end="\r",
+        )
+    print()
+
+    print(X.shape)
+    print(y.shape)
+
+
+if __name__ == "__main__":
+    classificator = Classificator()
+
+    INPUT_PATH = "test_mutation.json"
+
+    main(classificator)
