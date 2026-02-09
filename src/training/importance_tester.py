f2b201fd1816850c1a5e2f51ef22f094bba3ebf6
Bonifac <kolenomi@seznam.cz>
2026-02-09 19:23:31 +0100

    add chaotic mutation algoritm

--- src/training/importance_tester.py
+++ src/training/importance_tester.py
@@ -42,27 +42,35 @@ def single_revert(baseline: str, modified: str) -> tuple[list[str], list[int]]:
     return variants, diffs
 
 
-if __name__ == "__main__":
-    classificator = Classificator()
-    runner = ModelRunner(classificator)
+def generate_random_mutations(dna: str, value: float, num_mutations: int = 10):
+    amino_acids = "ACDEFGHIKLMNPQRSTVWY"
+    length = len(dna)
+    num_changes = max(1, int(length * value))
 
-    hypertermo = (
-        "MUT",
-        "ETLLIENFKSIRRLELKLRPGVNLLVGPNASGKTNILEAIYSFHKALFPPGVAQNSILATLAYVLEG",
-    )
+    mutations = []
 
-    hyperthermo_score = classificator.classify([hypertermo])[0][3]
-    print(f"Hyperthermo: {hyperthermo_score}")
-    predicted = runner.predict_importance(hypertermo[1])
+    for _ in range(num_mutations):
+        dna_list = list(dna)
+        positions = random.sample(range(length), num_changes)
 
+        for pos in positions:
+            original = dna_list[pos]
+            choices = [aa for aa in amino_acids if aa != original]
+            dna_list[pos] = random.choice(choices)
+
+        mutations.append("".join(dna_list))
+
+    return mutations
+
+
+def use_reverse_mutation(classificator, baseline, baseline_score):
     data = []
-
     for _ in range(5):
-        sinthetic_mezo, _, _ = reverse_mutate(classificator, hypertermo[1], 0.1, 0.1)
+        sinthetic_mezo, _, _ = reverse_mutate(classificator, baseline, 0.1, 0.1)
         if single_revert is None:
             print("didnt make it")
             continue
-        variants, indices = single_revert(sinthetic_mezo, hypertermo[1])
+        variants, indices = single_revert(sinthetic_mezo, baseline)
 
         mutations = [("", mut) for mut in variants]
         probability = [i[3] for i in classificator.classify(mutations)]
@@ -72,9 +80,9 @@ if __name__ == "__main__":
 
         single_data = []
         print(indices)
-        for idx, mut in enumerate(hypertermo[1]):
+        for idx, mut in enumerate(baseline):
             if idx == indices[counter]:
-                score = hyperthermo_score - probability[counter]
+                score = baseline_score - probability[counter]
                 counter += 1
             else:
                 score = 0
@@ -85,6 +93,60 @@ if __name__ == "__main__":
     real_score = np.mean(data, axis=0)
     print()
     print(real_score)
+    return real_score
+
+
+def use_chaotic_mutations(classificator, baseline, baseline_score):
+    percetage = 0.1
+    mutations = generate_random_mutations(baseline, percetage, 50)
+    data = []
+    for random_mutant in mutations:
+        if single_revert is None:
+            print("didnt make it")
+            continue
+        variants, indices = single_revert(random_mutant, baseline)
+
+        mutations = [("", mut) for mut in variants]
+        probability = [i[3] for i in classificator.classify(mutations)]
+
+        counter = 0
+        indices.append(-1)
+
+        single_data = []
+        print(indices)
+        for idx, mut in enumerate(baseline):
+            if idx == indices[counter]:
+                score = (
+                    baseline_score - probability[counter]
+                ) / percetage  # normalization
+                counter += 1
+            else:
+                score = 0
+            single_data.append(score)
+        # print(single_data)
+        data.append(single_data)
+
+    real_score = np.mean(data, axis=0)
+    print()
+    print(real_score)
+    return real_score
+
+
+if __name__ == "__main__":
+    classificator = Classificator()
+    runner = ModelRunner(classificator)
+
+    hypertermo = (
+        "MUT",
+        "MRSGLYAPPNWEYGSTMVVPPTMSSEEAETGGAG",
+    )
+
+    hyperthermo_score = classificator.classify([hypertermo])[0][3]
+    print(f"Hyperthermo: {hyperthermo_score}")
+    predicted = runner.predict_importance(hypertermo[1])
+
+    # real_score = use_reverse_mutation(classificator, hypertermo[1], hyperthermo_score)
+    real_score = use_chaotic_mutations(classificator, hypertermo[1], hyperthermo_score)
 
     make_importance_hyperthermo_compare(
         hypertermo, predicted, real_score, hyperthermo_score
