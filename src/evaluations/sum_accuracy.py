880f9622bb71a857f9237a8c778463090b5d6e3f
Bonifac <kolenomi@seznam.cz>
2026-04-16 15:44:16 +0200

    sum acc on train vs test data

--- src/evaluations/sum_accuracy.py
+++ src/evaluations/sum_accuracy.py
@@ -6,18 +6,9 @@ python -m src.evaluations.sum_accuracy
 """
 
 
-def aggregate_log_sigmoid(attribution):
-    s = np.abs(attribution).sum(axis=-1)
-    NORM_MEDIAN = -0.275390625
-    steepnes = 4
-    log_arr = np.log10(s)
-    return 1 / (1 + np.exp(-steepnes * (log_arr - NORM_MEDIAN)))
-
-
 X_PATH = "training_data/basic_1280/X.dat"
 Y_PATH = "training_data/basic_1280/y.dat"
 TOTAL_RESIDUES = os.path.getsize(Y_PATH)  # uint8 -> 1 byte per residue
-
 
 X = np.memmap(
     X_PATH,
@@ -27,17 +18,43 @@ X = np.memmap(
 )
 y = np.memmap(Y_PATH, dtype=np.uint8, mode="r", shape=(TOTAL_RESIDUES,))
 
+# create shuffled indices
+rng = np.random.default_rng(42)
+indices = rng.permutation(TOTAL_RESIDUES)
 
-s = aggregate_log_sigmoid(X)
 
-dom_acc_count = 0
-mut_acc_count = 0
+# 60:40 split
+split = int(0.6 * TOTAL_RESIDUES)
+train_idx = indices[:split]
+test_idx = indices[split:]
 
-for i in range(TOTAL_RESIDUES):
-    if y[i] == 0 and s[i] < 0.5:
-        dom_acc_count += 1
-    if y[i] == 1 and s[i] > 0.5:
-        mut_acc_count += 1
 
-print(dom_acc_count / (TOTAL_RESIDUES / 2))
-print(mut_acc_count / (TOTAL_RESIDUES / 2))
+def aggregate_log_sigmoid(attribution, norm):
+    s = np.abs(attribution).sum(axis=-1)
+    NORM_MEDIAN = -0.275390625
+    steepnes = 4
+    log_arr = np.log10(s)
+    return 1 / (1 + np.exp(-steepnes * (log_arr - norm)))
+
+
+def compute_acc(data, labels):
+    dom_acc_count = 0
+    mut_acc_count = 0
+
+    for i in range(data.shape[0]):
+        if labels[i] == 0 and data[i] < 0.5:
+            dom_acc_count += 1
+        if labels[i] == 1 and data[i] > 0.5:
+            mut_acc_count += 1
+
+    return (dom_acc_count + mut_acc_count) / data.shape[0]
+
+
+# --- TRAIN normalization ---
+s_train = np.abs(X[train_idx]).sum(axis=-1)
+norm = np.log10(np.median(s_train))
+
+print(f"Norm (train only): {norm}")
+print(compute_acc(aggregate_log_sigmoid(X[train_idx], norm), y[train_idx]))
+
+print(compute_acc(aggregate_log_sigmoid(X[test_idx], norm), y[test_idx]))
