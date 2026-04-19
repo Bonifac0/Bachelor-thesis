6f2c76ef5fbeac1e5b4647c9123c5ca0c15e85bf
Bonifac <kolenomi@seznam.cz>
2026-04-19 11:42:52 +0200

    agg hystogram for predictor

--- src/visualization/aggregation_hystograms.py
+++ src/visualization/aggregation_hystograms.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from src.training.run_model import ModelRunner
+import torch
+
 
 """
-python -m src.evaluations.aggregation_hystograms
+python -m src.visualization.aggregation_hystograms
 
 create hystogtam for sum function before and after transformation
+create hystogram of predictor output on dataset
 """
 
 X_PATH = "training_data/basic_1280/X.dat"
@@ -29,7 +33,7 @@ X = np.memmap(
 s = np.abs(X).sum(axis=-1)
 print(f"s mean: {s.mean()}")
 
-
+# sum before transformation
 bins = np.logspace(np.log10(s.min()), np.log10(s.max()), 100)
 plt.hist(s, bins=bins)
 plt.xscale("log")  # optional
@@ -40,6 +44,7 @@ plt.title("Histogram of Array Values")
 plt.savefig("histogram.png", dpi=300, bbox_inches="tight")
 plt.close()
 
+# sum after transformation
 norm = np.log10(np.median(s))
 print(f"Norm: {norm}")
 sigmoided = aggregate_log_sigmoid(s, norm)
@@ -51,4 +56,29 @@ plt.xlabel("Normalized Value (0-1)")
 plt.ylabel("Frequency")
 plt.title("Histogram (log-normalized)")
 plt.savefig("histogram_normalized.png", dpi=300, bbox_inches="tight")
+plt.close()
+
+# predictor
+runner = ModelRunner("2HL_64_16")
+model = runner.model
+
+# Compute embeddings
+inp = torch.from_numpy(X.copy()).float().to(runner.DEVICE)
+
+inp = (inp - runner.mean_emb) / runner.std_emb
+
+
+# Forward pass
+with torch.no_grad():
+    logits = runner.model(inp)
+    probs = torch.sigmoid(logits * 0.2)
+
+pred_s = probs.cpu().numpy()
+print(f"Median: {np.median(pred_s)}")
+
+plt.hist(pred_s, bins=100)
+plt.xlabel("Prediction")
+plt.ylabel("Frequency")
+plt.title("Histogram of Predictor")
+plt.savefig("histogram_predictor.png", dpi=300, bbox_inches="tight")
 plt.close()
